7f7a84edda7ed0c532e7344ff5af97db661dda95
Khail Saboor <khalilsaboor1@gmail.com>
2018-09-12 22:07:15 -0400

    Initial commit

---
+++ src/main/java/App.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class App {
+    public static void main(String [] args){
+        List<String> tempTxList = new ArrayList<String>();
+        tempTxList.add("Stevn => Khalil (+10)");
+        tempTxList.add("eric => Shivam (+100)");
+        tempTxList.add("khalil => eric(+33)");
+        tempTxList.add("Jake => Wanye(+200)");
+        tempTxList.add("Fin => Drake(+56)");
+        tempTxList.add("Travis => Kanye(+1)");
+
+        MerkleTrees merkleTrees = new MerkleTrees(tempTxList);
+        merkleTrees.merkle_tree();
+        System.out.println("This will be place in data column of our blocks");
+        System.out.println("root: " + merkleTrees.getRoot());
+    }
+  
+    }
+
---
+++ src/main/java/MerkleTrees.java
@@ -0,0 +1,100 @@
+import java.security.MessageDigest;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MerkleTrees {
+
+    //tranaction List
+    List<String> txList;
+
+    //Merkle Root
+    String root;
+
+    /**
+     *Constructor
+     * @param txList transaction List
+     */
+    public MerkleTrees(List<String> txList){
+        this.txList = txList;
+        root = "";
+    }
+
+    /**
+     * execute merkle_tree and set root.
+     */
+    public void merkle_tree(){
+        List<String> tempTxList = new ArrayList<String>();
+
+        for (int i = 0; i <this.txList.size() ; i++) {
+            tempTxList.add(this.txList.get(i));
+        }
+        List<String>newTxList = getNewTxList(tempTxList);
+        while (newTxList.size() != 1){
+            newTxList = getNewTxList(newTxList);
+        }
+        this.root = newTxList.get(0);
+    }
+
+    /**
+     * return Node Hash List.
+     * @param tempTxList
+     * @return
+     */
+
+    private List<String> getNewTxList(List<String> tempTxList) {
+        List<String> newTxList = new ArrayList<String>();
+        int index = 0;
+
+        while (index < tempTxList.size()){
+            //left
+            String left = tempTxList.get(index);
+            index++;
+
+            //right
+            String right = "";
+            if (index != tempTxList.size()){
+                right = tempTxList.get(index);
+            }
+
+            //Sha2 hex value
+            String sha2Hexvalue = getSHA2HexValue(left + right);
+            newTxList.add(sha2Hexvalue);
+            index++;
+        }
+        
+        return newTxList;
+    }
+
+    private String getSHA2HexValue(String str) {
+        byte[] cipher_byte;
+
+        try {
+            MessageDigest md = MessageDigest.getInstance("SHA-256");
+            md.update(str.getBytes());
+            cipher_byte = md.digest();
+            StringBuilder sb = new StringBuilder( 2 * cipher_byte.length);
+            for(byte b: cipher_byte){
+                sb.append(String.format("%02x", b&0xff) );
+            }
+            return sb.toString();
+            } catch (Exception e){
+            e.printStackTrace();
+        }
+
+         return "";
+
+    }
+    /**
+     * Get Root
+     * @return
+     */
+    public String getRoot(){
+        return this.root;
+    }
+    /** ■ Implementation Process
+
+     * Prepare for transaction data
+     * Create hash data combining left transaction and right transaction
+     * Execute loop until there is only one hash
+     */
+}
